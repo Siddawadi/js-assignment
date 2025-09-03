917ff6cf4aee0139b5b4a4dd5883cb0d31884fe5
siddharthadawadi <siddharthadawadi10@gmail.com>
2025-09-03 20:41:40 +0545

    indexjs for  merging

---
+++ index.js
@@ -0,0 +1,16 @@
+
+console.log('hello world');
+console.log(10+20);
+// ctrl + / to 
+ 
+var a = 20 ;
+var a = 30 ;
+var a = 40;
+console.log(a);
+ 
+let b = 23 
+ b = 32
+ console.log(b)
+
+ const c = 32
+ console.log(c)
